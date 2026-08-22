6f7f71b55134e0adfd7cd3a3136f2e1c4b6d63aa
KoderNmae <mr.wanderer95@gmail.com>
2026-08-22 18:57:07 +0300

    v3.7: объём по мышцам, серия, итоги месяца, замеры тела, CSV

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // офлайн-кэш: приложение открывается в зале даже без сети
-const C = 'ppl-v34';
+const C = 'ppl-v35';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting()));
 });

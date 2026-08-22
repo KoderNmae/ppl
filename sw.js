20817704174f4b09be0e245a8879b4bd5820ef6e
KoderNmae <mr.wanderer95@gmail.com>
2026-08-22 18:28:35 +0300

    v3.5.1: мускульный силуэт — цельный контур, органические формы

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // офлайн-кэш: приложение открывается в зале даже без сети
-const C = 'ppl-v32';
+const C = 'ppl-v33';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting()));
 });

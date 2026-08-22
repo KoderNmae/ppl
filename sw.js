e6c9024fae68bfa9ca9d9cca80632b70ecbf6b14
KoderNmae <mr.wanderer95@gmail.com>
2026-08-22 15:36:22 +0300

    v3.0.1: руки на рукоятях, рама тренажёра, пропорции карты мышц

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // офлайн-кэш: приложение открывается в зале даже без сети
-const C = 'ppl-v24';
+const C = 'ppl-v25';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting()));
 });

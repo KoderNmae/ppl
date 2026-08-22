59670c3fcfd87baac19f310df18d1f73f08dd6b7
KoderNmae <mr.wanderer95@gmail.com>
2026-08-22 17:32:17 +0300

    v3.4: плеер сразу в карточке (одно нажатие) + новая карта мышц

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // офлайн-кэш: приложение открывается в зале даже без сети
-const C = 'ppl-v29';
+const C = 'ppl-v30';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting()));
 });

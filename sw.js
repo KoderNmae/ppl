56be12b9a58b106ab6e0aee7dabcbbe14270805f
KoderNmae <mr.wanderer95@gmail.com>
2026-08-26 01:50:01 +0300

    v6.0: дизайн-реворк — сториз-кольца, единые панели, тёмная тема без белых пятен, чистка дублей

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // офлайн-кэш: приложение открывается в зале даже без сети
-const C = 'ppl-v53';
+const C = 'ppl-v60';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting()));
 });

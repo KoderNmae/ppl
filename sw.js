21d8f7fb647efdbf8ce32ac12ceebe127b1a43e7
KoderNmae <mr.wanderer95@gmail.com>
2026-08-26 18:46:17 +0300

    v6.4: 110 достижений в 7 коллекциях, 14 тем оформления с разблокировкой, персональные задачи недели

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // офлайн-кэш: приложение открывается в зале даже без сети
-const C = 'ppl-v63';
+const C = 'ppl-v64';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting()));
 });

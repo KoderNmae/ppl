83e65e6903520cfe63aa3e9f0c24a835e9624b46
KoderNmae <mr.wanderer95@gmail.com>
2026-08-27 01:55:49 +0300

    v6.9: большая чистка багов — журнал ошибок на устройстве + отчёт разработчику, фикс залипшей даты еды, звук таймера на iOS, склонения, оси графиков, страховки данных

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // офлайн-кэш: приложение открывается в зале даже без сети
-const C = 'ppl-v68';
+const C = 'ppl-v69';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting()));
 });

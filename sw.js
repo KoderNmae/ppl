4e6795a384f2e905d2f7ebcfd79ea6c4585623dd
KoderNmae <mr.wanderer95@gmail.com>
2026-08-27 02:20:03 +0300

    v7.0: скан не виснет и сканирует дальше, страница не прыгает вверх, свайп закрывает шторки, iOS-фикс «?», новая шкала тяжести, инвентарь на ходу, подписи и хелпы

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // офлайн-кэш: приложение открывается в зале даже без сети
-const C = 'ppl-v69';
+const C = 'ppl-v70';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting()));
 });

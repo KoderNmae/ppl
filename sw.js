7365c9dffe62be75c9b2faffabbd4d72b3c15fe9
KoderNmae <mr.wanderer95@gmail.com>
2026-08-26 11:50:49 +0300

    v6.1: магазинная полка 694 товара OFF со штрихкодами, офлайн-скан, скан→рецепты, поиск кириллица↔латиница

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // офлайн-кэш: приложение открывается в зале даже без сети
-const C = 'ppl-v60';
+const C = 'ppl-v61';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting()));
 });
